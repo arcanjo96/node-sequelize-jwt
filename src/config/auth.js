9731af4643692db73dec2b4a836a5d0ace565c72
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-10-14 18:04:46 -0300

    initial commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "00d352c038d0a63ca6b52aea71c7de74",
+  expiresIn: "7d"
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  port: "",
+  username: "",
+  password: "",
+  database: "",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
